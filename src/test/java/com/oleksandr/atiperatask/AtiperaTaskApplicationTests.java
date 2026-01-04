616f2c3ecfd38ac41ec0ae84663353449b14db1e
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2026-01-04 14:16:35 +0100

    initial commit

---
+++ src/test/java/com/oleksandr/atiperatask/AtiperaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleksandr.atiperatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
