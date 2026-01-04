03bcd36f2e6a5dfdf7da270291c245e5dfba8446
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2026-01-04 15:11:11 +0100

    Rewrote Branch from record to class with logic because had problem with sha: "branchList": [ { "name": "main", "sha": null } ] Waiting for basic github requests ( about 60 requests / hour as google saying ) limit before testing.

--- src/main/java/com/oleksandr/atiperatask/Branch.java
+++ src/main/java/com/oleksandr/atiperatask/Branch.java
@@ -1,7 +1,24 @@
 package com.oleksandr.atiperatask;
 
-public record Branch(
-        String name,
-        String sha
-) {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import java.util.Map;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Branch {
+
+    private String name;
+    private String sha;
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("commit")
+    private void unpackCommit(Map<String, Object> commit) {
+        this.sha = (String) commit.get("sha");
+    }
 }
