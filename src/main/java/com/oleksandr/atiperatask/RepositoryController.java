feeccc7d52f1e91c193e782766ffe60b13caacc2
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2026-01-04 14:56:52 +0100

    Implemented Repository Service for first tests...

--- src/main/java/com/oleksandr/atiperatask/RepositoryController.java
+++ src/main/java/com/oleksandr/atiperatask/RepositoryController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("v1/repository")
@@ -12,7 +14,7 @@ public class RepositoryController {
     private final RepositoryService service;
 
     @GetMapping("/{username}")
-    public ResponseEntity<RepositoryResponse> getUserReposByName(@PathVariable String username){
+    public ResponseEntity<List<RepositoryResponse>> getUserReposByName(@PathVariable String username){
         return ResponseEntity.ok(service.getUserRepos(username));
     }
 }
--- src/main/java/com/oleksandr/atiperatask/RepositoryService.java
+++ src/main/java/com/oleksandr/atiperatask/RepositoryService.java
@@ -1,11 +1,37 @@
 package com.oleksandr.atiperatask;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class RepositoryService {
-    public RepositoryResponse getUserRepos(String username) {
-        return null;
+
+    private final GitHubClient client;
+
+    public List<RepositoryResponse> getUserRepos(String username) {
+        var githubRepsonsesList = client.getUserRepos(username);
+        githubRepsonsesList = githubRepsonsesList
+                                            .stream()
+                                            .filter(elem -> elem.fork() == false)
+                                            .toList();
+        List<RepositoryResponse> repositoryResponseList = new ArrayList<>();
+        for( var el : githubRepsonsesList){
+
+            var branches = client.getBranchInfoByRepo(username, el.name());
+
+            RepositoryResponse response = new RepositoryResponse(
+                    el.name(),
+                    username,
+                    branches
+            );
+            repositoryResponseList.add(response);
+        }
+        return repositoryResponseList;
     }
 }
