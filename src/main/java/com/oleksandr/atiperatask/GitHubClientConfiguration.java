d06cd6e0b44e1851654480eb61246fa5bdd09e44
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2026-01-04 19:20:35 +0100

    Add variable for base url with DI from app.properties

--- src/main/java/com/oleksandr/atiperatask/GitHubClientConfiguration.java
+++ src/main/java/com/oleksandr/atiperatask/GitHubClientConfiguration.java
@@ -1,5 +1,6 @@
 package com.oleksandr.atiperatask;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -8,15 +9,17 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class GitHubClientConfiguration {
 
+    @Value("${github.api.url:https://api.github.com}")
+    private String githubApiUrl;
+
     @Bean
     public RestClient githubRestClient() {
         return RestClient.builder()
-                .baseUrl("https://api.github.com")
+                .baseUrl(githubApiUrl)
                 .defaultHeader(HttpHeaders.ACCEPT, "application/vnd.github+json")
                 .defaultHeader(HttpHeaders.USER_AGENT, "Atipera-APP_Kulbit")
                 .build();
     }
-
 }
 
 
